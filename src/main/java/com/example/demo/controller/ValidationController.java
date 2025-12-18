43fd42efe96134a5d5f9c9c5290401cdcdc8ab70
bala-amypo <bala@amypo.in>
2025-12-18 08:59:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.validation.Valid;
+
+
+@RestController
+public class  ValidationController{
+    @Autowired   ValidationService ser;
+
+    @PostMapping("/postted")
+    public  ValidationEntity sendData(@Valid @RequestBody  ValidationEntity stu){
+       return ser.postData(stu);
+    }
+   
