45446d75267b72077eb32acbb117d94e3f7e5489
bala-amypo <bala@amypo.in>
2025-12-19 09:46:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/IdcardEntity.java
+++ src/main/java/com/example/demo/entity/IdcardEntity.java
@@ -1,5 +1,25 @@
 package com.example.demo.entity;
 
+ 
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IdcardEntity{
-    private Integer
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    private int student_Id;
 }
--- src/main/java/com/example/demo/entity/StuEntity.java
+++ src/main/java/com/example/demo/entity/StuEntity.java
@@ -1,10 +1,30 @@
 package com.example.demo.entity;
 
+ 
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StuEntity{
+     @Id
+     @GeneratedValue(strategy=GenerationType.IDENTITY)
+
      private Integer id;
      private String name;
      private String email;
      private String address;
 
-     
+
 }
