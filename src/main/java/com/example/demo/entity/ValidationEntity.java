41c47239d0d8cb300884550a930f879b4eace311
bala-amypo <bala@amypo.in>
2025-12-20 02:25:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -39,53 +39,5 @@ public class ValidationEntity{
       @Min(18)
       @Positive(message="Age must be a positive number") 
       private int age;
-
-      
-//    public void setId(Long id){
-//       this.id=id;
-//    }
-//    public void setUsername(String username){
-//       this.username=username;
-//    }
-//    public void setPassword(String password){
-//       this.password=password;
-//    }
-//    public void setEmail(String email){
-//       this.email=email;
-//    }
-//    public void setAge(int age){
-//       this.age=age;
-//    }
-
-//    public Long getId(){
-//        return id;
-//    }
-//    public String getUsername(){
-//        return username;
-//    }
-//    public String getPassword(){
-//        return password;
-//    }
-//    public String getEmail(){
-//        return email;
-//   }
-//    public int getAge(){
-//        return age;
-//   }
-//   public ValidationEntity(){
-
-//   }
-//   public ValidationEntity(Long id,
-//           @NotNull @Size(min =2,max=6,message="must be 2 to 6 character") String username,
-//           @Email(message ="Email is not valid") String email,
-//           @Size(min=2 ,max=7,message="must be 2 to 7 character") @NotNull(message="Password is mandatory")
-//           String password, @Min(18) @Positive(message="Age must be a positive number")  int age){
-//         this.id=id;
-//         this.username=username;
-//         this.password=password;
-//         this.email=email;
-//         this.age=age;
-//           }
-
           
 }
